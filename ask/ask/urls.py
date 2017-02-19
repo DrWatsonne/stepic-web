49c226a0cedaaf012c9993dda6add871499b7fcf
DrWatsonne <watsonne@mail.ru>
2017-02-19 21:50:38 +1000

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -7,12 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^', include(qa.urls)),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', 'qa.views.test'),
-    url(r'^login/', 'qa.views.test'),
-    url(r'^signup/', 'qa.views.test'),
-    url(r'^ask/', 'qa.views.test'),
-    url(r'^popular/', 'qa.views.test'),
-    url(r'^new/', 'qa.views.test'),
+
 )
